df1503d8403ed3b7ddc61e51a6ae4be5f20d260b
Dominic DeCicco <ddecicco@buffalo.edu>
2018-12-07 19:43:42 -0700

    menu done :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {style, animate, transition, trigger } from '@angular/animations';
+import { Component, HostListener, ElementRef  } from '@angular/core';
+import {style, animate, state, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +7,14 @@ import {style, animate, transition, trigger } from '@angular/animations';
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('fadeInOut', [
-      transition(':enter', [   // :enter is alias to 'void => *'
-        style({opacity:0}),
-        animate(500, style({opacity:1})) 
-      ]),
-      transition(':leave', [   // :leave is alias to '* => void'
-        animate(500, style({opacity:0})) 
-      ])
+      state('show', style({
+        opacity: 0
+      })),
+      state('hide',   style({
+        opacity: 1
+      })),
+      transition('show => hide', [ style({opacity: 0}) , animate('800ms ease-in-out')]),
+      transition('hide => show',[ style({opacity: 1}), animate('800ms ease-in-out')] ) 
     ]),
     trigger('loaderAnimate', [
       transition(':enter', [   // :enter is alias to 'void => *'
@@ -32,11 +33,53 @@ import {style, animate, transition, trigger } from '@angular/animations';
       transition(':leave', [   // :leave is alias to '* => void'
         animate(500, style({display:'content'})) 
       ])
+    ]),
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+        transform: "translateY(0)"
+      })),
+      state('hide',   style({
+        opacity: 0,
+        transform: "translateY(-100%)"
+      })),
+      transition('show => hide', animate('500ms ease-out')),
+      transition('hide => show', animate('500ms ease-in'))
     ])
   ]
 })
 export class AppComponent {
 
-  title = 'wepack';
+  state = 'hide'
+
+  title = 'wepack'
+
+  menu_show = false
+  menu_hide = false
+
+  constructor(public el: ElementRef) {
+
+   }
+
+  @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+      const scrollPosition = window.pageYOffset
+
+      if (scrollPosition >= 20) {
+        this.state = 'show'
+      } else {
+        this.state = 'hide'
+      }
+
+    }
+
+    closeMenu(){
+      this.menu_hide = true
+      setTimeout(()=>{
+        this.menu_show = false
+        this.menu_hide = false
+      },900)
+    }
+    
 
 }
