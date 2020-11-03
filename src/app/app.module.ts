9083a643210ee50180336cc7c15cde6f7a369025
Andrew Chen <ac12491@gmail.com>
2020-11-03 13:51:20 -0700

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';;
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LazyLoadImagesModule } from 'ngx-lazy-load-images';
+
 @NgModule({
   declarations: [
     AppComponent
