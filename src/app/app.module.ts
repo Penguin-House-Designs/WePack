d72893731ba06432b4228fa63e4be41b4aebf1ad
Andrew Chen <ac12491@gmail.com>
2020-11-10 12:36:32 -0700

    redo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';;
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LazyLoadImagesModule } from 'ngx-lazy-load-images';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    CommonModule,
+    BrowserModule,
     BrowserAnimationsModule,
     LazyLoadImagesModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
